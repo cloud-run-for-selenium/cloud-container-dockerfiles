d8026be8b936ecc2dae0d5d424c00c590d668416
James Mortensen <jmort253@gmail.com>
2022-11-09 10:22:01 +0530

    Removed unneeded Node.js file.

--- .github/workflows/bin/get-release-notes.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const releaseNotesObj = JSON.parse(fs.readFileSync('./draft-release.json'));
-const releaseNotes = releaseNotesObj.Releases[0].body;
-
-console.log(releaseNotes + '\n\n');
-
